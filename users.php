9f8545471e376434bd1b5c0576b287bc841837e8
tauraspaj <pajuodis.tauras@gmail.com>
2021-04-27 13:22:52 +0100

    Remove seconds from timestamp

--- users.php
+++ users.php
@@ -337,7 +337,7 @@ include_once('header.php');
 					$result = mysqli_query($conn, $sql);
 					if ( mysqli_num_rows($result) > 0 ) {
 						while ($row = mysqli_fetch_assoc($result)) {
-							$alarmDate = 'Latest: '.$row['clearedAt'];
+							$alarmDate = 'Latest: '. substr($row['clearedAt'], 0, -3);
 						}
 					}
 				} else {
