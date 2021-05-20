b0da23353dcb8aab712df9ff5c4e523798e3f401
tauraspaj <pajuodis.tauras@gmail.com>
2021-05-20 12:15:06 +0100

    Only display first name in header

--- header.php
+++ header.php
@@ -424,7 +424,7 @@ if ($resultCheck > 0) {
 					<!-- Profile -->
 					<button id="openProfile" class="flex-none h-16 w-16 sm:w-auto flex items-center justify-center text-gray-700 hover:text-white border-r hover:bg-lightblue-400 hover:border-lightblue-400 focus:outline-none px-4">
 						<svg class="flex-none w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path></svg>
-						<p class="hidden sm:block whitespace-nowrap ml-2 text-sm"><?php echo $_SESSION['fullName'];?></p>
+						<p class="hidden sm:block whitespace-nowrap ml-2 text-sm"><?php echo explode(" ", $_SESSION['fullName'])[0];?></p>
 					</button>
 
 					<!-- Log out icon -->
