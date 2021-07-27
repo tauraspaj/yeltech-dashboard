6161678bf0841d0b82d4b382b4834624e5d62169
tauraspaj <pajuodis.tauras@gmail.com>
2021-07-27 10:00:56 +0100

    Search by email

--- includes/sqlUsersTable.php
+++ includes/sqlUsersTable.php
@@ -57,7 +57,7 @@ if ($function == 'showUsers') {
 	LEFT JOIN `groups` ON users.groupId = `groups`.groupId
 	LEFT JOIN sendingType ON users.sendingId = sendingType.sendingId
 	LEFT JOIN roles ON users.roleId = roles.roleId
-	WHERE ($roleSearch) AND ($sendingTypesSearch) AND (users.groupId = $groupFilter) AND (users.fullname LIKE '%$searchString%')
+	WHERE ($roleSearch) AND ($sendingTypesSearch) AND (users.groupId = $groupFilter) AND (users.fullname LIKE '%$searchString%' OR users.email LIKE '%$searchString%')
 	ORDER BY roleId ASC, groupName ASC, fullName ASC
 	LIMIT $devicesPerPage OFFSET $offset
 	";
@@ -74,7 +74,7 @@ if ($function == 'showUsers') {
 	
 	
 	$sqlTotal = "
-	SELECT COUNT(*) as totalRows FROM users WHERE ($roleSearch) AND ($sendingTypesSearch) AND (users.groupId = $groupFilter) AND (users.fullname LIKE '%$searchString%')
+	SELECT COUNT(*) as totalRows FROM users WHERE ($roleSearch) AND ($sendingTypesSearch) AND (users.groupId = $groupFilter) AND (users.fullname LIKE '%$searchString%' OR users.email LIKE '%$searchString%')
 	";
 	
 	$result = mysqli_query($conn, $sqlTotal);
