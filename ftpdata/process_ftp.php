6c97da31bbfaf2e32770865b24369b516f8d8673
tauraspaj <pajuodis.tauras@gmail.com>
2022-06-29 12:47:46 +0100

    Process FTP data

--- ftpdata/process_ftp.php
+++ ftpdata/process_ftp.php
@@ -3,23 +3,17 @@
 // Connect database
 require_once './../includes/dbh.inc.php';
 
-$data = file_get_contents('https://api2.yelcloud.com/getftpreadings/10');
+$data = file_get_contents('https://api2.yelcloud.com/getftpreadings/100');
 $data = json_decode($data, true);
 
-// Get latest KeyNum from database
-$sql = "
-    SELECT lastKeyNum FROM ftpTracker WHERE trackerId = 0
-";
-$result = mysqli_query($conn, $sql);
-if ( mysqli_num_rows($result) > 0 ) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $lastKeyNum = $row['lastKeyNum'];
-    }
-}
+$lastKeyNum = getLastKeyNum($conn);
+
 $updatedLastKeyNum = 0;
 
 // print_r($data);
-for ($i = count($data)-1; $i > 0; $i--) {
+// echo count($data);
+
+for ($i = count($data)-1; $i >= 0; $i--) {
     // Ensure current data being processed is not duplicate
     if ($data[$i]['KeyNum'] > $lastKeyNum) {
         $thisKeyNum = $data[$i]['KeyNum'];
@@ -45,9 +39,27 @@ for ($i = count($data)-1; $i > 0; $i--) {
 }
 
 // Update KeyNum with the latest value processed
-$sql = "
-    UPDATE ftpTracker SET lastKeyNum = $updatedLastKeyNum WHERE trackerId=0;
-";
-$result = mysqli_query($conn, $sql);
+if ($updatedLastKeyNum > 0) {
+    $sql = "
+        UPDATE ftpTracker SET lastKeyNum = $updatedLastKeyNum WHERE trackerId=0;
+    ";
+    $result = mysqli_query($conn, $sql);
+}
+
+
+function getLastKeyNum($conn) {
+    // Get latest KeyNum from database
+    $sql = "
+    SELECT lastKeyNum FROM ftpTracker WHERE trackerId = 0
+    ";
+    $result = mysqli_query($conn, $sql);
+    if ( mysqli_num_rows($result) > 0 ) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            if ($row['lastKeyNum'] > 0) {
+                return $row['lastKeyNum'];
+            }
+        }
+    }
+}
 
 ?>
